40bfb7bd4b0ef6e2f24825efd43412801d816ebf
otavioroecker <roeckerotavio@gmail.com>
2025-05-07 20:15:44 -0300

    desenvolvimento da navbar

--- Script/script.js
+++ Script/script.js
@@ -2,7 +2,14 @@ function abrirBarraLateral(){
     document.getElementById("barra-lateral").classList.add("ativa");
     document.getElementById("overlay").style.display = "block";
 }
-function fecharBarraLateral(){
+
+function abrirBarraBusca(){
+    document.getElementById("barra-busca").classList.add("ativa");
+    document.getElementById("overlay").style.display = "block";
+}
+
+function fecharTudo(){
     document.getElementById("barra-lateral").classList.remove("ativa");
-    document.getElementById("overlay").style.display = "none"
+    document.getElementById("barra-busca").classList.remove("ativa");
+    document.getElementById("overlay").style.display = "none";
 }
